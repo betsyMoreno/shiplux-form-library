2bff337eb22516f4bcf3966a2135217b7b1bbbce
betsy_Mb <morenoelba2002@gmail.com>
2023-03-31 16:14:01 -0400

    exporting default ES6 way

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-export const renderForm = (id, formProps) => {
-    const root = document.getElementById(id);
-    console.log(formProps, "first version")
-    root.innerHTML = <div>
-        <p>form test</p>
+const renderForm = (id, formProps) => {
+  const root = document.getElementById(id);
+  console.log(formProps, "first version");
+  root.innerHTML = (
+    <div>
+      <p>form test</p>
     </div>
-}
+  );
+};
+export default {
+  renderForm,
+};
